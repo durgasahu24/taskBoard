ac6d09ace1696fa23b1cbbc1fc2b9ed9c8f49aca
durgasahusahu <durgasahusahu1@gmail.com>
2025-10-08 15:22:40 +0530

    aded get req

--- server/index.js
+++ server/index.js
@@ -27,6 +27,10 @@ app.use("/api/task",taskRoute);
 
 
 
+app.get("/",(req,res) => {
+    return res.status(200).json({message:"hello server"});
+})
+
 connectdb()
 app.listen(port, () =>
     console.log("server is running on port no", port)
